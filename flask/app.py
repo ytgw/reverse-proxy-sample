71eb9a70dbb629ecb1f45280fd8efc20cb5d44a6
ytgw <tagawa0920@gmail.com>
2022-05-08 18:10:19 +0900

    HTTPヘッダーが表示されるように変更。

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Flask!'
+    html = '<h1>Hello Flask!</h1>\n'
+    for key, val in request.headers.items():
+        html += f'<p>{key}: {val}</p>\n'
+    return html
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
